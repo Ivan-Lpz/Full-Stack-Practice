7cacf266f8855d3dcdbee21fa66969b7503c79e2
Ivan Lopez <110691840+Ivan-Lpz@users.noreply.github.com>
2022-11-15 10:48:57 -0800

    First Python Project

---
+++ Python_Project/flask_app/static/homepage.js
@@ -0,0 +1,30 @@
+function rightArrowClick(){
+    var banner = document.querySelector("#banner");
+    banner.src = "/static/images/m4-rear.webp"; 
+}
+
+function leftArrowClick(){
+    var banner = document.querySelector("#banner");
+    banner.src = "/static/images/bmw-m4.jpeg"; 
+}
+
+function rightArrowMidClick(){
+    var banner = document.querySelector("#Midbanner");
+    banner.src = "/static/images/tesla-rear.jpeg"; 
+}
+
+function leftArrowMidClick(){
+    var banner = document.querySelector("#Midbanner");
+    banner.src = "/static/images/tesla-front.jpeg"; 
+}
+
+
+function botrightArrowClick(){
+    var banner = document.querySelector("#botbanner");
+    banner.src = "/static/images/camry-rear.jpeg"; 
+}
+
+function botleftArrowClick(){
+    var banner = document.querySelector("#botbanner");
+    banner.src = "/static/images/toyota-camry-front.jpeg"; 
+}
